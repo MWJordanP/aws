0bf5c73330b412612ee382bea4655aa5ec53b035
MWJordanP <j.prevot@mentalworks.fr>
2017-09-27 14:55:26 +0200

    update

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ use Embed\Embed;
 
 ?>
 
-<form action="" method="get">
-    <label for="">Url</label>
-    <input type="url" name="url">
-    <button>
-        Afficher
-    </button>
-</form>
+<!--<form action="" method="get">-->
+<!--    <label for="">Url</label>-->
+<!--    <input type="url" name="url">-->
+<!--    <button>-->
+<!--        Afficher-->
+<!--    </button>-->
+<!--</form>-->
 
 
 <?php
 
-if (isset($_GET['url']) && !empty($_GET['url'])) {
-    $info = Embed::create($_GET['url']);
+//if (isset($_GET['url']) && !empty($_GET['url'])) {
+    $info = Embed::create('https://www.youtube.com/watch?v=WxOulfAfKvk');
     echo $info->getCode();
-}
+//}
 
 ?>
