b55757d8f060c08cf9e8f361c19f79a712b0ab59
MWJordanP <j.prevot@mentalworks.fr>
2017-09-27 15:41:00 +0200

    update

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ use Embed\Embed;
 
 ?>
 
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 <!--<form action="" method="get">-->
 <!--    <label for="">Url</label>-->
 <!--    <input type="url" name="url">-->
@@ -12,13 +22,13 @@ use Embed\Embed;
 <!--        Afficher-->
 <!--    </button>-->
 <!--</form>-->
-
-
 <?php
 
 //if (isset($_GET['url']) && !empty($_GET['url'])) {
-    $info = Embed::create('https://www.youtube.com/watch?v=WxOulfAfKvk');
-    echo $info->getCode();
+$info = Embed::create('https://www.youtube.com/watch?v=WxOulfAfKvk');
+echo $info->getCode();
 //}
 
 ?>
+</body>
+</html>
